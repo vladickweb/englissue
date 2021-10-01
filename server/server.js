db54927fe1671846c331fa3389c6e792660ef1a2
Vladyslav <vladickbaraza@gmail.com>
2021-10-01 17:34:39 +0200

    added port from env

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const server = require('./app')
 const PORT = process.env.PORT || 3001
 
-server.listen(3001, () => {
+server.listen(PORT, () => {
 	console.log(`Server listening on port http://localhost:${PORT}`)
 })
